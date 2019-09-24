0f47b7e1d314af97463c02745cab698cf0c8585b
Axel Roffi <axel.roffi@gmail.com>
2019-09-24 13:45:27 +0200

    inbox deploy.js

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,40 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+const { interface, bytecode } = require("../compile");
+
+let accounts;
+let inbox;
+// const INITIAL_STRING = "Hello World!";
+beforeEach(async () => {
+  //get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+
+  //use one those accounts to deploy the smart Contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hello World!"] })
+    .send({ from: accounts[0], gas: "1000000" });
+  inbox.setProvider(provider);
+});
+
+describe("Inbox", () => {
+  it("deploys contract", () => {
+    assert.ok(inbox.options.address);
+  });
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hello World!");
+  });
+
+  it("can change message", async () => {
+    await inbox.methods.setMessage("Fuck Ethereum").send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Fuck Ethereum");
+  });
+});
 
 // class Car {
 //   park() {
